962a31608320b56b7718177066e40d1b7934c4e8
Paul Kim <paul@paulkim.net>
2025-01-07 22:28:09 -0500

    Add test page

--- lib/phoenix_inertia_web/controllers/page_controller.ex
+++ lib/phoenix_inertia_web/controllers/page_controller.ex
@@ -6,4 +6,8 @@ defmodule PhoenixInertiaWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def test(conn, _params) do
+    render(conn, :test)
+  end
 end
